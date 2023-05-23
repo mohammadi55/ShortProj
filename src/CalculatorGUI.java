46afcb69d3f6cbc02c9246db5e5a763e532c00f1
Mohammad <mohammadi55@nycstudents.net>
2023-05-23 09:29:17 -0400

    initial push

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
 
 
-public class CalculatorGUI extends JFrame {
+public class CalculatorGUI extends JFrame implements ActionListener, KeyListener {
 
     private JPanel mainPanel;
     private JButton Calculate;
@@ -9,7 +13,7 @@ public class CalculatorGUI extends JFrame {
     private JButton sinButton;
     private JButton cosButton;
     private JButton tanButton;
-    private JButton PWButton;
+    private JButton pwButton;
     private JButton Clear;
     private JButton logButton;
     private JButton lnButton;
@@ -51,4 +55,25 @@ public class CalculatorGUI extends JFrame {
 
         // TODO: place custom component creation code here
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
 }
